080b16c8d527c4370a3d8e79681bf248397218e1
Dios Vo <51939323+diosvo@users.noreply.github.com>
2021-01-06 22:50:59 +0700

    complete navbar components
    - animation

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,9 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  selected: number
+  private sizes = {
+    listItemWidth: 0,
+    translateX: 0
+  }
+
+  public navList = [
+    {
+      title: 'Home',
+      icon: 'icon-home',
+    },
+    {
+      title: 'Search',
+      icon: 'icon-magnifying-glass',
+    },
+    {
+      title: 'Like',
+      icon: 'icon-heart-outlined',
+    },
+    {
+      title: 'Profile',
+      icon: 'icon-user',
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get animate() {
+    return {
+      width: this.sizes.listItemWidth + 'px',
+      transform: `translateX(${this.sizes.translateX}px)`
+    }
+  }
+
+  updateSizes(element: HTMLElement, index = 1) {
+    this.sizes = {
+      listItemWidth: element.getBoundingClientRect().width,
+      translateX: element.getBoundingClientRect().width * index
+    }
+  }
+
+  onSelect(element: HTMLElement, i) {
+    this.selected = i;
+    this.updateSizes(element, i)
+  }
 }
