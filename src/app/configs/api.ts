b618b3ec736c92375c06c0dbf0314a7683731bb9
Dios Vo <51939323+diosvo@users.noreply.github.com>
2021-01-10 15:38:45 +0700

    Set up module: dynamic carousel gallery

--- src/app/configs/api.ts
+++ src/app/configs/api.ts
@@ -1,33 +1,38 @@
 export abstract class Configs {
-    public static listData = [
-        {
-            id: 1,
-            name: 'Navbar Interaction',
-            routerLink: '/components/navbar'
-        },
-        {
-            id: 2,
-            name: 'Liquid Menu Interaction',
-            routerLink: '/components/liquid-menu'
-        },
-    ]
+  public static listData = [
+    {
+      id: 1,
+      name: 'Navbar Interaction',
+      routerLink: '/components/navbar'
+    },
+    {
+      id: 2,
+      name: 'Liquid Menu Interaction',
+      routerLink: '/components/liquid-menu'
+    },
+    {
+      id: 3,
+      name: 'Dynamic Stacked Carousel Gallery',
+      routerLink: '/components/dynamic-carousel'
+    },
+  ]
 
-    public static navList = [
-        {
-          title: 'Home',
-          icon: 'icon-home',
-        },
-        {
-          title: 'Search',
-          icon: 'icon-magnifying-glass',
-        },
-        {
-          title: 'Like',
-          icon: 'icon-heart-outlined',
-        },
-        {
-          title: 'Profile',
-          icon: 'icon-user',
-        },
-      ];
+  public static navList = [
+    {
+      title: 'Home',
+      icon: 'icon-home',
+    },
+    {
+      title: 'Search',
+      icon: 'icon-magnifying-glass',
+    },
+    {
+      title: 'Like',
+      icon: 'icon-heart-outlined',
+    },
+    {
+      title: 'Profile',
+      icon: 'icon-user',
+    },
+  ];
 }
