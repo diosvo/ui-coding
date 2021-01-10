74864a9ca137dd322d989d06dbda1e850cf5be25
Dios Vo <51939323+diosvo@users.noreply.github.com>
2021-01-10 16:54:13 +0700

    Update static files

--- src/app/configs/api.ts
+++ src/app/configs/api.ts
@@ -1,3 +1,5 @@
+import { CarouselModel } from '../models/dynamic-carousel';
+
 export abstract class Configs {
   public static listData = [
     {
@@ -33,6 +35,37 @@ export abstract class Configs {
     {
       title: 'Profile',
       icon: 'icon-user',
+    },
+  ];
+
+  public static carouselList: CarouselModel[] = [
+    {
+      title: 'ManUtd - Catona #7',
+      url: '../assets/images/carousel/catona.jpg',
+    },
+    {
+      title: 'Doctor Strange',
+      url: '../assets/images/carousel/doctor-strange.jpg',
+    },
+    {
+      title: 'Dwyane Wade',
+      url: '../assets/images/carousel/dw.jpg',
+    },
+    {
+      title: 'King Eric Catona',
+      url: '../assets/images/carousel/eric.jpg',
+    },
+    {
+      title: 'Paul Scholes #18',
+      url: '../assets/images/carousel/scholes.jpg',
+    },
+    {
+      title: 'Miami Heat - Wade #3',
+      url: '../assets/images/carousel/wade.jpg',
+    },
+    {
+      title: 'White Chocolate - Jason William #55',
+      url: '../assets/images/carousel/william.jpg',
     },
   ];
 }
