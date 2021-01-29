31113ea9fa7286a1e85c5b145b6a57ff14fc7b6c
Dios Vo <51939323+diosvo@users.noreply.github.com>
2021-01-29 13:20:16 +0700

    Complete styles .  Stash it (can not set CSSRule button::before)

--- src/app/pages/button/micro-interaction/micro-interaction.component.ts
+++ src/app/pages/button/micro-interaction/micro-interaction.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+import CSSRulePlugin from "gsap/dist/CSSRulePlugin";
 
 @Component({
   selector: 'app-micro-interaction',
@@ -10,6 +12,38 @@ export class MicroInteractionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.mainFunction()
   }
 
+  mainFunction() {
+    gsap.registerPlugin(CSSRulePlugin);  
+    
+    const tl = gsap.timeline({
+      defaults: {
+        ease: 'easeOut',
+        delay: 1
+      }
+    })
+
+    const btn = document.querySelector('button')
+    const rule = document.querySelector('button::before')
+
+    // Set tl: timeline
+    tl.to('.label', {
+      opacity: 0,
+      height: 0,
+      position: 'absolute',
+      duration: '.2s'
+    })
+
+    tl.to(btn, {
+      borderRadius: '50%',
+      width: '2.5em',
+      height: '2.5em'
+    }, '-=.5s')
+
+    tl.to(rule, {
+      borderRadius: '50%',
+    }) 
+  }
 }
