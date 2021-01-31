00ae2ce1dff2dd29b11e7972c5fb450d7b7236aa
Dios Vo <51939323+diosvo@users.noreply.github.com>
2021-01-31 16:03:28 +0700

    Import dayjs & set up config file (fake data, images)

---
+++ src/app/configs/estate/menuList.ts
@@ -0,0 +1,31 @@
+export const estateMenuList = [
+    {
+        name: 'Dashboard',
+        icon: 'icon-grid',
+        link: '/'
+    },
+    {
+        name: 'Data',
+        icon: 'icon-circular-graph',
+        link: '/data'
+    },
+    {
+        name: 'Listings',
+        icon: 'icon-list',
+        link: '/listings'
+    },
+    {
+        name: 'Clients',
+        icon: 'icon-user',
+        link: '/clients'
+    },
+    {
+        name: 'Bills',
+        icon: 'icon-text-document',
+        link: '/bills'
+    }, {
+        name: 'Settings',
+        icon: 'icon-cog',
+        link: '/settings'
+    },
+];
--- src/app/web/estate/estate.component.ts
+++ src/app/web/estate/estate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { estatePropertyList } from 'src/app/configs/estate/propertyList';
 
 @Component({
   selector: 'app-estate',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./estate.component.scss']
 })
 export class EstateComponent implements OnInit {
-
+  estateMenuList =  estatePropertyList
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.estateMenuList);
+    
   }
 
 }
