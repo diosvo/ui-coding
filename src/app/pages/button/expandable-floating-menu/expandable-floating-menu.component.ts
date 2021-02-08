6cd3d1c3bc662c5b614a0182bcce5bd8c294a726
Dios Vo <51939323+diosvo@users.noreply.github.com>
2021-02-09 01:56:07 +0700

    Complete static html

--- src/app/pages/button/expandable-floating-menu/expandable-floating-menu.component.ts
+++ src/app/pages/button/expandable-floating-menu/expandable-floating-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { menuDataList } from '../../../configs/expandable-floating-menu/menu-data'
 
 @Component({
   selector: 'app-expandable-floating-menu',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expandable-floating-menu.component.scss']
 })
 export class ExpandableFloatingMenuComponent implements OnInit {
-
+  menuData = menuDataList
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.menuData);
+    
   }
 
 }
