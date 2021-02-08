1130a3297baaef6aa4fd1973e6bee31bd6290904
Dios Vo <51939323+diosvo@users.noreply.github.com>
2021-02-09 01:55:24 +0700

    File config for module expandable-floating-menu

---
+++ src/app/configs/expandable-floating-menu/menu-data.ts
@@ -0,0 +1,24 @@
+import { MenuModel } from 'src/app/models/expandable-floating-menu';
+
+export const menuDataList: MenuModel[] = [
+    {
+        menuTitle: 'Laptop',
+        menuImageUrl: 'https://images.unsplash.com/photo-1593642634315-48f5414c3ad9?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+        menuTitle: 'Flowers',
+        menuImageUrl: 'https://images.unsplash.com/photo-1612733021445-b258ad12a5dd?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60',
+    },
+    {
+        menuTitle: 'Cake',
+        menuImageUrl: 'https://images.unsplash.com/photo-1612724880140-e7456ea477b6?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3OXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60',
+    },
+    {
+        menuTitle: 'Eagle',
+        menuImageUrl: 'https://images.unsplash.com/photo-1612723652267-7565bb0ac539?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80',
+    },
+    {
+        menuTitle: 'Coffee',
+        menuImageUrl: 'https://images.unsplash.com/photo-1612714401296-87a1768bd741?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80',
+    },
+];
