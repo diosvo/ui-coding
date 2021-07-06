ee54661ce8b73e88a8e43036d49e5de6c0da6d82
Dios Vo <vtmn1212@gmail.com>
2021-07-06 14:09:02 +0700

    Add currency pipe

--- src/app/functions/rxjs/data-composition-ng-conf/data-composition-ng-conf.module.ts
+++ src/app/functions/rxjs/data-composition-ng-conf/data-composition-ng-conf.module.ts
@@ -5,8 +5,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { AlertModule } from 'src/assets/shared/components/alert/alert.module';
 
+import { AlertModule } from 'src/assets/shared/components/alert/alert.module';
 import { ContentComponent } from './components/content/content.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
