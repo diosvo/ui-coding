79878d60dace7526c2ed9d20ef32059a09889bdf
Dios Vo <vtmn1212@gmail.com>
2021-07-07 23:19:19 +0700

    Implement basic logic & UI

--- src/app/web-ui/table/advanced-crud/advanced-crud.module.ts
+++ src/app/web-ui/table/advanced-crud/advanced-crud.module.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { AdvancedCrudRoutingModule } from './advanced-crud-routing.module';
 import { AdvancedCrudComponent } from './components/advanced-crud/advanced-crud.component';
-
 @NgModule({
   declarations: [
     AdvancedCrudComponent
@@ -20,7 +22,10 @@ import { AdvancedCrudComponent } from './components/advanced-crud/advanced-crud.
 
     MatTableModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatTooltipModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class AdvancedCrudModule { }
