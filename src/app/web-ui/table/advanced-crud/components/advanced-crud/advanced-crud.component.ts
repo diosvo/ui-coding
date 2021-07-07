19a741cbfa09858ad7c521c1c4cad9ba766b7e39
Dios Vo <vtmn1212@gmail.com>
2021-07-08 00:10:36 +0700

    implement UI

--- src/app/web-ui/table/advanced-crud/components/advanced-crud/advanced-crud.component.ts
+++ src/app/web-ui/table/advanced-crud/components/advanced-crud/advanced-crud.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-advanced-crud',
@@ -6,10 +8,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advanced-crud.component.scss']
 })
 export class AdvancedCrudComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name', 'action'];
+  dataSource = new MatTableDataSource<any>();
 
-  constructor() { }
+  form: FormGroup = this.fb.group({
+    rows: this.fb.array([])
+  });
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void { }
+
+  addNewRow(): void {
+    this.rows.push(this.initRow());
+    this.dataSource = new MatTableDataSource(this.rows.controls);
+    console.log(this.dataSource);
+    
   }
 
+  editItem(idx: number): void {
+    this.rows.at(idx).get('isEditable').patchValue(false);
+  }
+
+  deleteItem(idx: number): void {
+    this.rows.removeAt(idx);
+  }
+
+  saveChanges(idx: number): void {
+    this.rows.at(idx).get('isEditable').patchValue(true);
+  }
+
+  cancelChanges(idx: number): void {
+    this.rows.at(idx).get('isEditable').patchValue(true);
+  }
+
+  private get rows(): FormArray {
+    return this.form.get('rows') as FormArray;
+  }
+
+  private initRow(): FormGroup {
+    return this.fb.group({
+      categoryId: [null, Validators.required],
+      categoryName: [null, Validators.required],
+      isEditable: [false]
+    });
+  }
 }
